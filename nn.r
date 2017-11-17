a4bce6aaa82a374bd53f4b56509adf336b03d3a0
Henrique Fonseca <henrique.c.fonseca@tecnico.ulisboa.pt>
2017-11-17 19:57:55 +0000

    neural network Noshows done, not done for crabs (error)

--- nn.r
+++ nn.r
@@ -2,15 +2,18 @@ library(class)
 library(MASS)
 library(unbalanced)
 library(lubridate)
+library(caret) 
 library(nnet)
 
-data(crabs, package = "MASS")
+# -----------------------------NOSHOWS--------------------------------------- #
 noshows = read.csv("noshows.csv")
 
 #import function from pre-process file
 source("./pre-process.r")
 
 preprocessed_noshows <- preprocessNoshows(noshows, 50)
+
+
 #normalize noshows dataset only (scheduledday, appointment, age, neighbourhood) 
 normalized_noshows <- cbind(as.data.frame(lapply(preprocessed_noshows[,c(1,2,3,4,5)], normalize)), preprocessed_noshows[,c(6,7,8,9,10,11,12)])
 
@@ -20,10 +23,12 @@ shuffleData = function(d){
   return(shuffledData)
 }
 
+#DIVIDE AND SHUFFLE dataset into a training set of 90% and 10% test set -----**
 data <- normalized_noshows
 shuffle <- TRUE
-testSize <- nrow(data) * 0.1
+testSize <- nrow(data) * 0.05
 
+#random seed = meaning of life
 set.seed(42)
 sample_size = 0
 if(shuffle){
@@ -33,16 +38,26 @@ if(shuffle){
 }
 train = data[-sample_size]
 n = ncol(data)
-print(n)
+
+#training set that goes into training of network
 proto_train = data[-sample_size, ]
 proto_test = data[sample_size, ]
 
+#results90 unused (probably wrongly calculated)
+#results10: Column ShowUp of the last 10%
 results90 <- train[,ncol(train)]
 results10 <- proto_test[,ncol(proto_test)]
 
-nnetwork.nn2 <- nnet(ShowUp ~ ., data = proto_train, size=10, decay = 5e-4, maxit = 200)
+#Train Neural Networl, Formula = ShowUp, input data, number of hidden layers, etc
+nnetwork.nn2 <- nnet(ShowUp ~ ., data = proto_train, size=9, decay = 0, maxit = 300)
+#Use previous network to classify ShowUp column of proto_test
 prediction <- predict(nnetwork.nn2, proto_test, type = "class")
 
-table(prediction, results10)
+#compare predicted results with real ShowUp column from test set
+t <- table(prediction, results10)
+confusionMatrix(t)
                  
-                 
+# ------------------------------------CRABS----------------------------------------- #
+
+data(crabs, package = "MASS")
+
